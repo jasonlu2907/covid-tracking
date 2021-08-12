4580e1fa360a76d226f12da1f118a4234b9b167c
Jason Lu <jasonlu2907@gmail.com>
2021-08-11 20:16:48 -0500

    Created globalData datatype, created a tracking-sevice hence retrieve data from an API and turned it into an array of values, setting routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackingService } from '../services/tracking.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  data: any;
+  totalConfirmed: number = 0;
+  totalDeath: number = 0;
+  totalRecovered: number = 0;
+  totalActive: number = 0;
 
-  constructor() { }
+  constructor(private trackingService: TrackingService) { }
 
   ngOnInit(): void {
+    this.trackingService.getData().subscribe(res => {
+      console.log(res);
+    });
   }
+
+  
 
 }
--- src/app/services/tracking.service.ts
+++ src/app/services/tracking.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GlobalData } from '../models/global-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackingService {
+  private dataURL: string = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-09-2021.csv';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getData(): Observable<any> {
+    return this.http.get(this.dataURL, {responseType: 'text'})
+      .pipe(map(
+        res => {
+          //Store the data in the globalDetails Obj { *countryname: <globalData>}
+          const globalDetails: any = {};
+
+          let rows = res.split('\n');
+          // console.log(rows);
+
+          //Remove the first element because it is just the definitions of each col
+          rows.splice(0, 1);
+          rows.forEach(row => {
+            //split khi gặp dấu , except the , with the next element is a blankspace
+            let cols = row.split(/,(?=\S)/);
+            const eachReport = {
+              country: cols[3],
+              confirmed: +cols[7], // TRICK: Doi string -> integer: add +
+              death: +cols[8],
+              recovered: +cols[9],
+              active: +cols[10]
+            };
+
+            let temp = globalDetails[eachReport.country];
+            if(temp) {
+              temp.confirmed += eachReport.confirmed;
+              temp.death += eachReport.death;
+              temp.recovered += eachReport.recovered;
+              temp.active += eachReport.active;
+
+              // this line doesn't affect the app
+              globalDetails[eachReport.country] = temp;
+            } else {
+              globalDetails[eachReport.country] = eachReport;
+            }
+          });
+
+          // console.log(globalDetails);
+          //Vietnam: {"country": "Vietnam","confirmed": 1234,...}
+          return Object.values(globalDetails); //return as an array with the values, 
+          //since we don't care about the key
+        }
+      ));
+  }
 }
